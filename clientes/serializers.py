95a60e722beb109f6f14ebaf911b89f005021c54
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-25 23:52:11 -0300

    Adicionando o readme e um serielizer

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,3 +6,29 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf):
+        if len(cpf) != 11:
+            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
+        
+        return cpf
+    
+    def validate_nome(self, nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError("Não inclua números no nome")
+
+        return nome
+    
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve ter 9 Dígitos")
+
+        return rg
+    
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O Celular deve ter 11 Dígitos")
+        
+        return celular
+        
+
+    
